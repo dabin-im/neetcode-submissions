0ff69ec6743b2ceca148ca874b2987eb97a2b480
Dabin Im <112664372+dabin-im@users.noreply.github.com>
2026-05-13 12:57:43 +0900

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nMap = {}
+
+        for i in range(len(nums)):
+            k = target - nums[i]
+            
+            if k in nMap:
+                return [nMap[k], i]
+            
+            nMap[nums[i]] = i
+
+        return []
